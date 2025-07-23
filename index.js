657eb46d62e1a1a85cb9e2d9e7e2f8d00a68611d
Scamwac <wachannarkhede4@gmail.com>
2025-07-23 23:25:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //hello
+const obj = {
+  value: 42,
+  func: function() {
+    console.log(this.value); // 42
+  }
+};
+obj.func();
+//Regular Function: Has its own this, which depends on how the function is called (e.g., global object, object method, or explicit binding with call/apply)
+
+const obj1 = {
+  value: 42,
+  func: () => {
+    console.log(this.value); // undefined
+  }
+};
