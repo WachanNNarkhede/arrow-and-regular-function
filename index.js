e72be9e2623897f9b7e85ab2dbda464f7f94701c
Scamwac <wachannarkhede4@gmail.com>
2025-07-23 23:25:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ const obj1 = {
     console.log(this.value); // undefined
   }
 };
+obj1.func();
+// arrow Inherits this from the surrounding lexical scope (no own this). Useful in callbacks where you want to retain the outer this.
+
+// Regular Function: Has an arguments object containing all passed arguments.
+
+function myFunction() {
+  console.log(arguments); // [1, 2, 3]
+}
+myFunction(1, 2, 3);
+
+//Arrow Function: No arguments object. Use rest parameters (...args) instead.
+const myFunction = (...args) => {
+  console.log(args); // [1, 2, 3]
+};
